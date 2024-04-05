40894e84dc68d88f5c4100f5b57951a527e4d2e3
eduardoVela <eduardovela2022@icloud.com>
2024-04-05 09:14:42 -0600

    added functionality to the collectEmployees function

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 // Get a reference to the #add-employees-btn element
-const addEmployeesBtn = document.querySelector('#add-employees-btn');
+const addEmployeesBtn = document.querySelector("#add-employees-btn");
 
 // Collect employee data
-const collectEmployees = function() {
-  // TODO: Get user input to create and return an array of employee objects
-}
+const collectEmployees = function () {
+  // Stores all the employees that were created by the function
+  let employees = [];
+  // Checks if the user wants to continue using the function
+  let isCreatingEmployee = true;
+
+  while (isCreatingEmployee) {
+    // Gets the employee's information
+    let firstName = getString(
+      "Enter the employee's first name:",
+      "Please enter a valid employee’s first name:"
+    );
+    let lastName = getString(
+      "Enter the employee's last name:",
+      "Please enter a valid employee’s last name:"
+    );
+    let salary = getNumber("Enter the employee's salary:");
+
+    // Creates the employee object
+    let employee = {
+      firstName,
+      lastName,
+      salary,
+    };
+
+    // Stores the new employee object in the array of employees
+    employees.push(employee);
+
+    // Checks if the user wants to add another employee to the array
+    isCreatingEmployee = confirm("Want to add another employee?");
+  }
+
+  // Returns the list of employees
+  return employees;
+};
 
 // Display the average salary
-const displayAverageSalary = function(employeesArray) {
+const displayAverageSalary = function (employeesArray) {
   // TODO: Calculate and display the average salary
-}
+};
 
 // Select a random employee
-const getRandomEmployee = function(employeesArray) {
+const getRandomEmployee = function (employeesArray) {
   // TODO: Select and display a random employee
-}
+};
 
 /*
   ====================
@@ -23,12 +55,12 @@ const getRandomEmployee = function(employeesArray) {
 */
 
 // Display employee data in an HTML table
-const displayEmployees = function(employeesArray) {
+const displayEmployees = function (employeesArray) {
   // Get the employee table
-  const employeeTable = document.querySelector('#employee-table');
+  const employeeTable = document.querySelector("#employee-table");
 
   // Clear the employee table
-  employeeTable.innerHTML = '';
+  employeeTable.innerHTML = "";
 
   // Loop through the employee data and create a row for each employee
   for (let i = 0; i < employeesArray.length; i++) {
@@ -46,29 +78,29 @@ const displayEmployees = function(employeesArray) {
 
     const salaryCell = document.createElement("td");
     // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
-      style:"currency",
-      currency:"USD"
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
     });
 
     newTableRow.append(salaryCell);
 
     employeeTable.append(newTableRow);
   }
-}
+};
 
-const trackEmployeeData = function() {
+const trackEmployeeData = function () {
   const employees = collectEmployees();
 
   console.table(employees);
 
   displayAverageSalary(employees);
 
-  console.log('==============================');
+  console.log("==============================");
 
   getRandomEmployee(employees);
 
-  employees.sort(function(a,b) {
+  employees.sort(function (a, b) {
     if (a.lastName < b.lastName) {
       return -1;
     } else {
@@ -77,7 +109,47 @@ const trackEmployeeData = function() {
   });
 
   displayEmployees(employees);
-}
+};
 
 // Add event listener to 'Add Employees' button
-addEmployeesBtn.addEventListener('click', trackEmployeeData);
+addEmployeesBtn.addEventListener("click", trackEmployeeData);
+
+// Validates a string
+function getString(message, errorMessage) {
+  // Gets the string
+  let string = prompt(message);
+  // Checks if string is not empty
+  let error = true;
+
+  while (error) {
+    // If string is empty or null get the string again
+    if (!string) {
+      string = prompt(errorMessage);
+      error = true;
+    }
+    // If string is valid continue
+    else {
+      error = false;
+    }
+  }
+
+  // Returns the string
+  return string;
+}
+
+// Validates a number
+function getNumber(message) {
+  // Gets the number
+  let number = prompt(message, 0);
+
+  // If number is not a number return 0
+  if (isNaN(number)) {
+    return 0;
+  }
+
+  // If number is a number return it
+  return number;
+}
+
+// https://www.w3schools.com/js/js_popup.asp
+// https://www.w3schools.com/jsref/jsref_isnan_number.asp#:~:text=isNaN()%20returns%20true%20if,a%20number%20before%20testing%20it.
